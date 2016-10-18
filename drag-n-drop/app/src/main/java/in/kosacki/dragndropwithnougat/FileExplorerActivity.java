947695682ad9aa144c81fc0f5d5355cf9327df96
Hubert Kosacki <hubert.kosacki@codete.com>
2016-10-18 11:09:49 +0200

    onSaveInstanceState

--- drag-n-drop/app/src/main/java/in/kosacki/dragndropwithnougat/FileExplorerActivity.java
+++ drag-n-drop/app/src/main/java/in/kosacki/dragndropwithnougat/FileExplorerActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -126,11 +127,11 @@ public class FileExplorerActivity extends AppCompatActivity {
         }
     }
 
-    //    @Override
-    //    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-    //        outState.putString(LAST_LOCATION_KEY, currentPath.getPath());
-    //        super.onSaveInstanceState(outState, outPersistentState);
-    //    }
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putString(LAST_LOCATION_KEY, currentPath.getPath());
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
